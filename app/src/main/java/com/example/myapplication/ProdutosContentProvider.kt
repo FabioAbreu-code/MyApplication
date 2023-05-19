b990e0dce5e1a5282c0e32e5c9746da1ef160dce
Fabio Abreu <fabiofiqueli@hotmail.com>
2023-05-19 10:46:18 +0100

    Criado o UriMatcher para validar os endereços do content provider

--- app/src/main/java/com/example/myapplication/ProdutosContentProvider.kt
+++ app/src/main/java/com/example/myapplication/ProdutosContentProvider.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -43,5 +44,19 @@ class ProdutosContentProvider : ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val  AUTHORITY = "com.example.myapplication"
+        const val PRODUTOS = "produtos"
+        const val STOCK = "stock"
+
+        private const val URI_PRODUTOS = 100
+        private const val URI_STOCK = 200
+
+        fun  uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTHORITY, PRODUTOS, URI_PRODUTOS)
+            addURI(AUTHORITY, STOCK, URI_STOCK)
+        }
     }
 }
