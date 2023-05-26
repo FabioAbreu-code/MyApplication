b281462b3dc5c18b8af237a5a21d3413a5912643
Fabio Abreu <fabiofiqueli@hotmail.com>
2023-05-26 18:07:59 +0100

    Corrigido problema do binding

--- app/src/main/java/com/example/myapplication/ListaProdutosFragment.kt
+++ app/src/main/java/com/example/myapplication/ListaProdutosFragment.kt
@@ -35,8 +35,12 @@ class ListaProdutosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> 
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_produtos, container, false)
+        _binding = FragmentListaProdutosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
