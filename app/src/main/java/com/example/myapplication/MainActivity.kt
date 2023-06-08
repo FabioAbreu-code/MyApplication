b2259abbdf99f654e24cf95f1d994c6a3e0d361b
Fabio Abreu <fabiofiqueli@hotmail.com>
2023-06-08 14:25:09 +0100

    Processar as opções do menu da lista de produtos no fragmento correspondente

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -53,10 +53,18 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+
+        if (item.itemId == R.id.action_settings) {
+            return true
         }
+
+        val opcaoProcessada = when (fragment) {
+            is ListaStockFragment -> (fragment as ListaStockFragment).processaOpcaoMenu(item)
+            else -> false
+        }
+
+        return if (opcaoProcessada) { true } else { super.onOptionsItemSelected(item) }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
