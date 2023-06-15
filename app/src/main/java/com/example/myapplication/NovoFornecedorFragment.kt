e38d51b7c9e674e132999f42fc073d5c7467931f
Fabio Abreu <fabiofiqueli@hotmail.com>
2023-06-15 16:34:19 +0100

    Guardar fornecedor

--- app/src/main/java/com/example/myapplication/NovoFornecedorFragment.kt
+++ app/src/main/java/com/example/myapplication/NovoFornecedorFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentNovoFornecedorBinding
+
 
 class NovoFornecedorFragment : Fragment() {
     private var _binding: FragmentNovoFornecedorBinding? = null
@@ -46,18 +48,49 @@ class NovoFornecedorFragment : Fragment() {
                 true
             }
             R.id.action_cancelar -> {
-                cancelar()
+                voltaListaFornecedores()
                 true
             }
             else -> false
         }
     }
 
-    private fun cancelar() {
+    private fun voltaListaFornecedores() {
         findNavController().navigate(R.id.action_novoFornecedorFragment_to_listaFornecedoresFragment)
     }
 
     private fun guardar() {
+        val nome_fornecedor = binding.editTextNomeFornecedor.text.toString()
+        if (nome_fornecedor.isBlank()) {
+            binding.editTextNomeFornecedor.error = getString(R.string.nome_do_fornecedor_obrigatorio)
+            binding.editTextNomeFornecedor.requestFocus()
+            return
+        }
 
+        val contacto_fornecedor = binding.editTextContactoFornecedor.text.toString()
+        if (contacto_fornecedor.isBlank()) {
+            binding.editTextContactoFornecedor.error = getString(R.string.contacto_fornecedor_obrigatorio)
+            binding.editTextContactoFornecedor.requestFocus()
+            return
+        }
+
+
+        val fornecedor = Fornecedores(
+            nome_fornecedor,
+            contacto_fornecedor
+        )
+
+        val id = requireActivity().contentResolver.insert(
+            ProdutoContentProvider.ENDERECO_FORNECEDOR,
+            fornecedor.toContentValues()
+        )
+
+        if (id == null) {
+            binding.editTextNomeFornecedor.error = getString(R.string.erro_guardar_fornecedor)
+            return
+        }
+
+        Toast.makeText(requireContext(), getString(R.string.fornecedor_guardado_com_sucesso), Toast.LENGTH_SHORT).show()
+        voltaListaFornecedores()
     }
 }
